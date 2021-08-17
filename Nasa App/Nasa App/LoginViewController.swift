1d081c2d42fe0c7cf963565077bf5e091d931651
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-08-17 16:03:44 -0300

    View controller rename

--- Nasa App/Nasa App/LoginViewController.swift
+++ Nasa App/Nasa App/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var background: UIView!
     @IBOutlet weak var fieldsView: UIView!
