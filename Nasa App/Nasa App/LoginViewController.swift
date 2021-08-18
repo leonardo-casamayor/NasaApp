36de1fd3393a74eb9d6b2f8ba84e97604a1b5173
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-08-18 18:11:29 -0300

    Fixed keyboard behaviour and added navigation base

--- Nasa App/Nasa App/LoginViewController.swift
+++ Nasa App/Nasa App/LoginViewController.swift
@@ -8,37 +8,54 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    @IBOutlet weak var background: UIView!
-    @IBOutlet weak var fieldsView: UIView!
-    @IBOutlet weak var buttonsView: UIView!
+    
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var registerButton: UIButton!
-    @IBOutlet weak var containerView: UIView!
-    @IBOutlet weak var padding1: UIView!
-    @IBOutlet weak var padding2: UIView!
-    
+    @IBOutlet weak var buttonsView: UIView!
+    @IBOutlet weak var userField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        let blueBackground: [UIView] = [background, fieldsView, buttonsView, containerView, padding1, padding2]
-
-        blueBackground.forEach { view in
-            applyBackgroundColor(view)
-        }
-        
+        setBackgroundColor(self.view)
+        setBackgroundColor(buttonsView)
         formatButtons(loginButton)
         formatButtons(registerButton)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        setupTextFields()
+
+        
     }
     
-    func applyBackgroundColor(_ view: UIView) {
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        textField.endEditing(true)
+//        return true
+//    }
+    
+    func setupTextFields() {
+            let toolbar = UIToolbar()
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                            target: nil, action: nil)
+            let doneButton = UIBarButtonItem(title: "Done", style: .done,
+                                             target: self, action: #selector(doneButtonTapped))
+            
+            toolbar.setItems([flexSpace, doneButton], animated: true)
+            toolbar.sizeToFit()
+            
+            userField.inputAccessoryView = toolbar
+            passwordField.inputAccessoryView = toolbar
+        }
         
+        @objc func doneButtonTapped() {
+            view.endEditing(true)
+        }
+    
+    func setBackgroundColor (_ view: UIView) {
         view.backgroundColor = UIColor(red:0.02, green:0.24, blue:0.58, alpha:1)
-        
     }
+    
     func formatButtons(_ view: UIView) {
         
         view.layer.cornerRadius = 5
@@ -47,7 +64,19 @@ class LoginViewController: UIViewController {
         view.layer.borderColor = UIColor(red:0.59, green:0.59, blue:0.59, alpha:1).cgColor
 
     }
+    
+    @IBAction func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
 
-
+    @IBAction func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
 }
 
