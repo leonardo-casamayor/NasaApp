d10a5f61c12525dcf720e049b468508339d956f6
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-08-19 14:21:04 -0300

    Fixed constraints and added inspectable properties

--- Nasa App/Nasa App/LoginViewController.swift
+++ Nasa App/Nasa App/LoginViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: MyButton!
+    @IBOutlet weak var registerButton: MyButton!
     @IBOutlet weak var buttonsView: UIView!
     @IBOutlet weak var userField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -22,35 +22,14 @@ class LoginViewController: UIViewController {
         setBackgroundColor(buttonsView)
         formatButtons(loginButton)
         formatButtons(registerButton)
+        setupTextFields()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        setupTextFields()
-
+        
         
     }
     
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        textField.endEditing(true)
-//        return true
-//    }
-    
-    func setupTextFields() {
-            let toolbar = UIToolbar()
-            let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
-                                            target: nil, action: nil)
-            let doneButton = UIBarButtonItem(title: "Done", style: .done,
-                                             target: self, action: #selector(doneButtonTapped))
-            
-            toolbar.setItems([flexSpace, doneButton], animated: true)
-            toolbar.sizeToFit()
-            
-            userField.inputAccessoryView = toolbar
-            passwordField.inputAccessoryView = toolbar
-        }
-        
-        @objc func doneButtonTapped() {
-            view.endEditing(true)
-        }
+    //MARK: Set Colors
     
     func setBackgroundColor (_ view: UIView) {
         view.backgroundColor = UIColor(red:0.02, green:0.24, blue:0.58, alpha:1)
@@ -58,11 +37,29 @@ class LoginViewController: UIViewController {
     
     func formatButtons(_ view: UIView) {
         
-        view.layer.cornerRadius = 5
         view.backgroundColor = UIColor(red:0.95, green:0.56, blue:0.55, alpha:1)
-        view.layer.borderWidth = 1
         view.layer.borderColor = UIColor(red:0.59, green:0.59, blue:0.59, alpha:1).cgColor
-
+        
+    }
+    
+    //MARK: Keyboard Behaviour
+    
+    func setupTextFields() {
+        let toolbar = UIToolbar()
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                        target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done,
+                                         target: self, action: #selector(doneButtonTapped))
+        
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        toolbar.sizeToFit()
+        
+        userField.inputAccessoryView = toolbar
+        passwordField.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButtonTapped() {
+        view.endEditing(true)
     }
     
     @IBAction func keyboardWillShow(notification: NSNotification) {
@@ -72,7 +69,7 @@ class LoginViewController: UIViewController {
             }
         }
     }
-
+    
     @IBAction func keyboardWillHide(notification: NSNotification) {
         if self.view.frame.origin.y != 0 {
             self.view.frame.origin.y = 0
