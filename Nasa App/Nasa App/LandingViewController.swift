0dfd4c18750e8bda9e23b74788b4d5ff9e1245bf
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-08-20 14:04:17 -0300

    Added views

---
+++ Nasa App/Nasa App/LandingViewController.swift
@@ -0,0 +1,34 @@
+//
+//  LandingViewController.swift
+//  Nasa App
+//
+//  Created by David Felipe Lizarazo Velandia on 10/08/21.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var textView: UITextView!
+    
+    let nasaBlueUI = UIColor(red:0.02, green:0.24, blue:0.58, alpha:1)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = nasaBlueUI
+        textView.backgroundColor = nasaBlueUI
+        makeGradient(gradientView)
+        
+    }
+    
+    func makeGradient(_ view: UIView) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.clear.cgColor, nasaBlueUI.cgColor]
+        view.backgroundColor = UIColor.clear
+        view.layer.addSublayer(gradientLayer)
+    }
+
+}
+
