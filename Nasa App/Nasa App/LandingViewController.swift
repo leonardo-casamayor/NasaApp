caa3c855a176e09939892689676f3a74e39ff19a
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-08-23 11:52:00 -0300

    Corrections

--- Nasa App/Nasa App/LandingViewController.swift
+++ Nasa App/Nasa App/LandingViewController.swift
@@ -11,30 +11,31 @@ class LandingViewController: UIViewController {
 
     @IBOutlet weak var gradientView: UIView!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
     
-    let nasaBlueUI = UIColor(red:0.02, green:0.24, blue:0.58, alpha:1)
+    let nasaBlue = UIColor(red:0.02, green:0.24, blue:0.58, alpha:1)
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        view.backgroundColor = nasaBlueUI
-        textView.backgroundColor = nasaBlueUI
-        
+        view.backgroundColor = nasaBlue
+        textView.backgroundColor = nasaBlue
     }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+        gradientView.layer.sublayers?.forEach { $0.removeFromSuperlayer() }
         makeGradient(gradientView)
-
     }
+    
     private func makeGradient(_ view: UIView) {
-        
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
-        gradientLayer.colors = [UIColor.clear.cgColor, nasaBlueUI.cgColor]
+        gradientLayer.colors = [UIColor.clear.cgColor, nasaBlue.cgColor]
         view.backgroundColor = UIColor.clear
         view.layer.addSublayer(gradientLayer)
-        
     }
+    
 }
 
