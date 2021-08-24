c933e6484a3b5f0fcf8da993b66d768249551dd0
JuliusGlob <julio.robles@globant.com>
2021-08-24 11:25:18 -0300

    Solving problems with rotation after implementing swiftui on storyboard

--- Nasa App/Nasa App/Views/CellDetailView/CellDetailViewController.swift
+++ Nasa App/Nasa App/Views/CellDetailView/CellDetailViewController.swift
@@ -10,14 +10,43 @@ import UIKit
 import SwiftUI
 
 class CellDetailViewController: UIViewController {
-    
-    @IBOutlet weak var viewContainer: UIView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let childView = UIHostingController(rootView: CellDetailView())
-        addChild(childView)
-        childView.view.frame = viewContainer.bounds
-        viewContainer.addSubview(childView.view)
+        addSwiftUIView()
+    }
+    
+    private func addSwiftUIView() {
+        let swiftUIView = CellDetailView()
+        addSubSwiftUIView(swiftUIView, to: view)
+    }
+}
+
+
+extension UIViewController {
+
+    
+    func addSubSwiftUIView<Content>(_ swiftUIView: Content, to view: UIView) where Content : View {
+        let hostingController = UIHostingController(rootView: swiftUIView)
+
+        // Add as a child of the current view controller.
+        addChild(hostingController)
+
+        // Add the SwiftUI view to the view controller view hierarchy.
+        view.addSubview(hostingController.view)
+
+        // Setup the contraints to update the SwiftUI view boundaries.
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leftAnchor.constraint(equalTo: view.leftAnchor),
+            view.bottomAnchor.constraint(equalTo: hostingController.view.bottomAnchor),
+            view.rightAnchor.constraint(equalTo: hostingController.view.rightAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+
+        // Notify the hosting controller that it has been moved to the current view controller.
+        hostingController.didMove(toParent: self)
     }
 }
