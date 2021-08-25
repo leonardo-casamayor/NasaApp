fc10f28a5eb6992e6d83012c0270ace2c6a92b85
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-08-25 14:58:19 -0300

    Constraint corrections

--- Nasa App/Nasa App/LandingViewController.swift
+++ Nasa App/Nasa App/LandingViewController.swift
@@ -14,27 +14,29 @@ class LandingViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var explanationLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    private var gradientLayer: CAGradientLayer?
     let nasaBlue = UIColor(red:0.02, green:0.24, blue:0.58, alpha:1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = nasaBlue
         explanationLabel.backgroundColor = nasaBlue
+        makeGradient(gradientView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        gradientView.layer.sublayers?.forEach { $0.removeFromSuperlayer() }
-        makeGradient(gradientView)
+        gradientLayer?.frame = gradientView.bounds
     }
     
     private func makeGradient(_ view: UIView) {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [UIColor.clear.cgColor, nasaBlue.cgColor]
+        gradientLayer = CAGradientLayer()
+        gradientLayer?.colors = [UIColor.clear.cgColor, nasaBlue.cgColor]
         view.backgroundColor = UIColor.clear
-        view.layer.addSublayer(gradientLayer)
+        guard let layer = gradientLayer else { return }
+        view.layer.addSublayer(layer)
     }
     
 }
