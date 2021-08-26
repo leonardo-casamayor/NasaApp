fe3336088d3e6c9f9b9ee05119e3a93ad0d2085a
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-08-26 15:20:33 -0300

    First commit to swiftui view controller

--- Nasa App/Nasa App/Detail.swift
+++ Nasa App/Nasa App/Detail.swift
@@ -6,30 +6,76 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct Detail: View {
+    
+    @Environment(\.horizontalSizeClass) var sizeClass
+    
     var body: some View {
-        ZStack {
-            Spacer().ignoresSafeArea().background(Color.blue)
-        GeometryReader { geo in
-            VStack {
-            Text("Video view area")
-                .frame(width: geo.size.width, height: geo.size.height * 0.45, alignment: .center)
-                .background(Color.red)
-                ScrollView {
-                    Text("Scroll view area")
-                        
-                }.frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity, minHeight: 0, idealHeight: 100, maxHeight: .infinity, alignment: .center)
-                .background(Color.red)
+        
+        let fontSize = sizeClass == .compact ? smallFont : largeFont
+        
+        NavigationView {
+            ZStack {
                 
+                Spacer()
+                    .background(nasaBlue)
+                    .edgesIgnoringSafeArea(.bottom)
+                    .edgesIgnoringSafeArea(.horizontal)
+                
+                GeometryReader { geo in
+                    
+                    let screenWidth = geo.size.width
+                    let screenHeight = geo.size.height
+                    let isPortrait = screenWidth < screenHeight
+                    let videoHiehgt = isPortrait ? (screenWidth * videoAspectRatio) : screenHeight
+                    
+                    VStack {
+                        
+                        ZStack {
+                            Image("example")
+                                .resizable()
+                                .scaledToFit()
+                                .background(Color.clear)
+                            if isPortrait {
+                                gradient(color: nasaBlue)
+                            }
+                            VStack {
+                                if isPortrait {
+                                    Spacer()
+                                    titleText(screenWidth, withTextFontSize: fontSize.titleFontSize)
+                                }
+                            }
+                            Button{} label: {
+                                Label("", systemImage: "play.fill")
+                            }
+                        }
+                        .frame(width: screenWidth, height: videoHiehgt, alignment: .center)
+                        
+                        if isPortrait {
+                            scrollView(screenWidth, withTextFontSize: fontSize.descriptionFontSize)
+                        }
+                        
+                    }
+                }
+                
+                //Navigation View Settings
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarLeading) {
+                        Button("Close"){}
+                    }
+                    ToolbarItemGroup{
+                        Button{}
+                            label: {
+                                Label("favorite", systemImage: "heart")
+                            }
+                    }
+                }
+            }
         }
-            
-        }
-        .background(Color.blue)
-        .edgesIgnoringSafeArea(.top)
-            
-
-        }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -37,4 +83,30 @@ struct Detail_Previews: PreviewProvider {
     static var previews: some View {
         Detail()
     }
+}
+
+private func scrollView(_ width: CGFloat, withTextFontSize: CGFloat) -> some View {
+    return ScrollView {
+        Text(mock)
+            .font(.system(size: withTextFontSize))
+            .fontWeight(.semibold)
+            .padding(.top)
+            .frame(width: width * 0.9)
+            .foregroundColor(.white)
+        
+    }
+}
+private func titleText(_ width: CGFloat, withTextFontSize: CGFloat) -> some View {
+    return Text("Title / Date")
+        .foregroundColor(.white)
+        .font(.system(size: withTextFontSize))
+        .fontWeight(.bold)
+        .frame(maxWidth: width * 0.93, alignment: .leading)
+}
+
+private func gradient(color: Color) -> some View {
+    return Rectangle()
+        .fill(
+            LinearGradient(gradient: Gradient(colors: [.clear, color]), startPoint: .top, endPoint: .bottom))
+        .padding(.top)
 }
---
+++ Nasa App/Nasa App/DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  Nasa App
+//
+//  Created by Leonardo Casamayor on 26/08/2021.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSwiftUIView()
+    }
+    
+    private func addSwiftUIView() {
+        let swiftUIView = Detail()
+        addSubSwiftUIView(swiftUIView, to: view)
+    }
+}
+
+extension UIViewController {
+
+    func addSubSwiftUIView<Content>(_ swiftUIView: Content, to view: UIView) where Content : View {
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let constraints = [
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leftAnchor.constraint(equalTo: view.leftAnchor),
+            view.bottomAnchor.constraint(equalTo: hostingController.view.bottomAnchor),
+            view.rightAnchor.constraint(equalTo: hostingController.view.rightAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
+
