0d04c8d73bd52cb537f67e3395ffc45db35f01bc
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-09-01 22:01:57 -0300

    Added search bar

--- Nasa App/Nasa App/CollectionViewController.swift
+++ Nasa App/Nasa App/CollectionViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 class CollectionViewController: UIViewController, UISearchControllerDelegate, UISearchBarDelegate {
     
     private let collectionView = UICollectionView(frame: .zero,collectionViewLayout: CollectionViewController.generateLayout(size: LayoutSize(columns: 2, height: 1/3)))
+    private let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         setupCollectionViewController()
     }
     
     private func setupCollectionViewController() {
-        self.view.backgroundColor = nasaBlue
         view.addSubview(collectionView)
+        view.backgroundColor = nasaBlue
+        setupSearchBar()
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
         collectionView.register(CollectionCell.self, forCellWithReuseIdentifier: CollectionCell.identifier)
-        collectionViewConstraints()
         collectionView.setCollectionViewLayout(CollectionViewController.generateLayout(size: self.setLayoutSize()), animated: false)
         collectionView.backgroundColor = .clear
         collectionView.dataSource = self
+        collectionViewConstraints()
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.titleView = searchController.searchBar
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.definesPresentationContext = true
     }
     
     private func collectionViewConstraints() {
@@ -30,7 +42,7 @@ class CollectionViewController: UIViewController, UISearchControllerDelegate, UI
         collectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
         collectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
     }
 }
 
@@ -52,12 +64,13 @@ extension CollectionViewController: UICollectionViewDataSource {
 extension CollectionViewController {
     
     private func setLayoutSize() -> LayoutSize {
-        return traitCollection.horizontalSizeClass == .compact ? LayoutSize(columns: 1,height: 2/3) : LayoutSize(columns: 2,height: 1/3)
+        return (traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular) ? LayoutSize(columns: 1,height: 2/3) : LayoutSize(columns: 2,height: 1/3)
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         super.traitCollectionDidChange(previousTraitCollection)
         collectionView.setCollectionViewLayout(CollectionViewController.generateLayout(size: setLayoutSize()), animated: false)
+        collectionView.setContentOffset(.zero, animated: true)
     }
     
     fileprivate static func generateLayout(size: LayoutSize) -> UICollectionViewCompositionalLayout {
