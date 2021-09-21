702f0e2fdcfe7a69a0fae2006e23cc6f26a8f0b7
JuliusGlob <julio.robles@globant.com>
2021-09-21 11:33:05 -0300

    implementing basic structure

---
+++ Nasa App/Nasa App/Login/Users.swift
@@ -0,0 +1,13 @@
+//
+//  Users.swift
+//  Nasa App
+//
+//  Created by Julio Ismael Robles on 21/09/2021.
+//
+
+import Foundation
+
+struct Users: Codable {
+    let username: String
+    let password: String
+}
