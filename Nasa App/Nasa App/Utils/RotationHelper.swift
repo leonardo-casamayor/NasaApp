8c5377b66ae2de2f1122dbc0cb295af80fadad46
leonardo.casamayor <leonardo.casamayor@globant.com>
2021-09-24 18:33:48 -0300

    Removed unnecessary code

--- Nasa App/Nasa App/Utils/RotationHelper.swift
+++ Nasa App/Nasa App/Utils/RotationHelper.swift
@@ -13,10 +13,4 @@ struct RotationHelper {
             delegate.orientationLock = orientation
         }
     }
-
-    static func lockOrientation(_ orientation: UIInterfaceOrientationMask, andRotateTo rotateOrientation:UIInterfaceOrientation) {
-        self.lockOrientation(orientation)
-        UIDevice.current.setValue(rotateOrientation.rawValue, forKey: "orientation")
-        UINavigationController.attemptRotationToDeviceOrientation()
-    }
 }
