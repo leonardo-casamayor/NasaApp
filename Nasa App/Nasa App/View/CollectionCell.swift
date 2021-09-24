afb2644c8f0443fd0312535acc826bdc2bcf3724
JuliusGlob <julio.robles@globant.com>
2021-09-24 10:31:35 -0300

    more fixes on font

--- Nasa App/Nasa App/View/CollectionCell.swift
+++ Nasa App/Nasa App/View/CollectionCell.swift
@@ -78,8 +78,9 @@ class CollectionCell: UICollectionViewCell {
     }
     func configureCellWith(data: [NasaData]) {
         self.dateLabel.text = DateFormat.formatDate(dateString: data[0].dateCreated)
-        self.titleLabel.text = data[0].title.lowercased().trunc(length: 30).capitalized
-        self.titleLabel.font = (data[0].title.count > 30) ? UIFont.systemFont(ofSize: 22, weight: .semibold) : UIFont.systemFont(ofSize: 28, weight: .semibold)
+        self.titleLabel.text = data[0].title.lowercased().trunc(length: 32).capitalized
+        self.titleLabel.font = (data[0].title.count > 25) ? UIFont.systemFont(ofSize: 22, weight: .semibold) : UIFont.systemFont(ofSize: 28, weight: .semibold)
+        self.titleLabel.adjustsFontSizeToFitWidth = true
     }
 }
 
