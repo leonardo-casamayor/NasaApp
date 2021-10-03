21702f1ad3ff4ad53a4932e2a10d6ea37478f15f
JuliusGlob <julio.robles@globant.com>
2021-10-03 20:31:06 -0300

    several qol fixes on landing view controller

--- Nasa App/Nasa App/ApodModel.swift
+++ Nasa App/Nasa App/ApodModel.swift
@@ -6,54 +6,21 @@
 //
 
 import Foundation
-import UIKit
 
 struct APODElement: Codable {
     let date, explanation: String
     let hdurl: String?
     let mediaType: ApodMediaType
     let title: String
-    let url: String?
+    let url: String
     let copyright: String?
     let thumbnailUrl: String?
-    var apodImage: UIImage?
-    
+
     enum CodingKeys: String, CodingKey {
         case date, explanation, hdurl, title, url, copyright
         case mediaType = "media_type"
         case thumbnailUrl = "thumbnail_url"
     }
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.url = try? container.decodeIfPresent(String.self, forKey: .url)
-        guard let stringUrl = self.url else { return }
-        guard let url: URL = URL(string: stringUrl) else { return }
-        loadImage(url) { result in
-            
-            apodImage = result
-        }
-    }
-    
-    mutating func loadImage(_ urlString: URL, completion: @escaping (_ Image: UIImage) -> Void) {
-        
-        let session = URLSession(configuration: .default)
-        
-        let downloadImageTask = session.dataTask(with: urlString) { (data, _, error) in
-            if let error = error {
-                print(error.localizedDescription)
-            } else {
-                guard let imageData = data, let image = UIImage(data: imageData)  else {
-                    completion(#imageLiteral(resourceName: "Loading"))
-                    return
-                }
-                    
-                    completion(image)
-                    
-                }
-            session.finishTasksAndInvalidate()
-            }
-        downloadImageTask.resume()
-        }
 }
 
 enum ApodMediaType: String, Codable {
@@ -63,8 +30,3 @@ enum ApodMediaType: String, Codable {
 
 
 typealias Apod = [APODElement]
-
-
-    
-
-
--- Nasa App/Nasa App/Controller/LandingViewController.swift
+++ Nasa App/Nasa App/Controller/LandingViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import NVActivityIndicatorView
 
 class LandingViewController: UIViewController {
     
@@ -19,21 +18,20 @@ class LandingViewController: UIViewController {
     
     private var gradientLayer: CAGradientLayer?
     let nasaBlue = UIColor(red:0.02, green:0.24, blue:0.58, alpha:1)
-    var activtyIndicator: NVActivityIndicatorView?
+    var networkManager = NetworkManager()
     var animate = true
     var apod: APODElement? {
-        didSet{
-            guard let apod = apod else { return }
-            let url = apod.mediaType == ApodMediaType.image ? apod.url : apod.thumbnailUrl
-            dataImage = url
-            loadData(data: apod)
+        didSet {
+            guard let apodData = apod else { return }
+            loadData(data: apodData)
+            prepareImage(data: apodData)
         }
     }
-    var dataImage: String? {
+    var apodImageString: String? {
         didSet {
             DispatchQueue.main.async {
-                print(self.dataImage!)
-                self.image.loadImage(self.apod!.url)
+                guard let imageUrl = self.apodImageString else { return }
+                self.image.setImageFrom(imageUrl)
             }
         }
     }
@@ -42,19 +40,16 @@ class LandingViewController: UIViewController {
         super.viewDidLoad()
         setupViewController()
         DispatchQueue.global(qos: .userInteractive).async {
-            self.fetchData(url: NetworkManagerConstants.apodAPIURL){ [weak self] result in
-                guard let strongSelf = self else {return}
+            self.networkManager.retrieveApodData { [weak self] result in
+                guard let strongSelf = self else { return }
                 switch result {
                 case .success(let data):
-                strongSelf.apod = data
+                    strongSelf.apod = data
                 case .failure(_):
-                    print("error")
+                    strongSelf.loadData(data: LandingConstants.apodMock, isMockData: true)
                 }
             }
-
         }
-                    
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -68,9 +63,6 @@ class LandingViewController: UIViewController {
         titleLabel.fadeIn(1, delay: 0)
         subtitleLabel.fadeIn(1, delay: 0.5)
         explanationLabel.fadeIn(1, delay: 1)
-        activtyIndicator = NVActivityIndicatorView(frame: image.frame, type: .orbit, color: .white, padding: 200)
-        guard let activityIndicatorU = self.activtyIndicator else { return }
-        gradientView.addSubview(activityIndicatorU)
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -101,20 +93,23 @@ extension LandingViewController {
         let copyright = data.copyright ?? ""
         let date = data.date.replacingOccurrences(of: "/", with: "-")
         let subtitle = copyright != "" ? "\(copyright.trunc(length: 25))  / \(date)" : date
-//        let url = data.mediaType == ApodMediaType.image ? data.url : data.thumbnailUrl
-//        if !isMockData {
-//            guard let imageUrl = url else { return}
-//            self.dataImage = imageUrl
-//        }
+        
         DispatchQueue.main.async { [weak self] in
             guard let strongSelf = self else { return }
             strongSelf.titleLabel.text = data.title
             strongSelf.subtitleLabel.text = subtitle
             strongSelf.explanationLabel.text = data.explanation
-//            if isMockData {
-//                strongSelf.image.image = #imageLiteral(resourceName: "apod-example")
-//            }
+            /// use a generic image if it's mockdata
+            if isMockData {
+                strongSelf.image.image = #imageLiteral(resourceName: "apod-example")
+            }
         }
+    }
+    
+    private func prepareImage(data: APODElement) {
+        let url = data.mediaType == ApodMediaType.image ? data.url : data.thumbnailUrl
+        guard let imageUrl = url else { return}
+        self.apodImageString = imageUrl
     }
 }
 
@@ -131,63 +126,3 @@ extension UIView {
         })
     }
 }
-extension LandingViewController {
-    func fetchData(url: String, completion: @escaping (Result<APODElement, Error>) -> Void) {
-        guard let url = URL(string: url) else { return }
-        let session = URLSession.shared
-        let request = URLRequest(url: url)
-        let task = session.dataTask(with: request){ (data, response, error) in
-            
-            guard let dataUnwrap = data else { return }
-            
-            if let httpResponse = response as? HTTPURLResponse {
-                
-                let stausCode = httpResponse.statusCode
-                switch stausCode {
-                case 200..<300:
-                    do {
-                        let decoder = JSONDecoder()
-                        decoder.dateDecodingStrategy = .iso8601
-                        let decodedObject = try decoder.decode(APODElement.self, from: dataUnwrap)
-                        completion(.success(decodedObject))
-                    } catch {
-                        print("parsing error")
-                    }
-                default:
-                    print("Unexpected Error")
-                }
-            }
-        }
-        task.resume()
-    }
-    
-}
-extension UIImageView {
-    func loadImage(_ urlString: String, completion: (() -> Void)? = nil) {
-        DispatchQueue.global(qos: .userInteractive).async {
-            guard let url = URL(string: urlString) else { return }
-            
-            let session = URLSession(configuration: .default)
-            
-            let downloadImageTask = session.dataTask(with: url) { (data, response, error) in
-                if let error = error {
-                    print(error.localizedDescription)
-                } else {
-                    if let imageData = data {
-                        DispatchQueue.main.async {[weak self] in
-                            var image = UIImage(data: imageData)
-                            self?.image = nil
-                            self?.image = image
-                            image = nil
-                            completion?()
-                        }
-                    }
-                }
-                session.finishTasksAndInvalidate()
-            }
-            downloadImageTask.resume()
-        }
-        }
-        
-}
-
